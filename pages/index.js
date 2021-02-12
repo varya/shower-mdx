215a96acb08f6e5d552a0cd2f23f777fe062596d
Irina Illustrova <illustrova@gmail.com>
2021-02-12 02:20:45 +0200

    ERemove unused imports

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import fs from "fs";
 import React, { useEffect, useRef } from "react";
 import matter from "gray-matter";
 import renderToString from "next-mdx-remote/render-to-string";
-import Head from "next/head";
-import path from "path";
 import * as Components from "../components";
 
 const components = {
@@ -11,8 +9,7 @@ const components = {
   ...Components,
 };
 
-export default function Presentation({ content, frontMatter, ...props }) {
-  console.log("🚀 ~ file: index.js ~ line 15 ~ Presentation ~ props", props);
+export default function Presentation({ content, frontMatter }) {
   const ref = useRef(null);
 
   useEffect(() => {
@@ -20,6 +17,7 @@ export default function Presentation({ content, frontMatter, ...props }) {
     const takeFocus = () => ref.current.focus();
     document.getElementById("__next").setAttribute("tabindex", "-1");
     document.getElementById("__next").addEventListener("focus", takeFocus);
+
     // TODO: temporary hack - adding class to body in order to get access to color variables, search for better solution
     document.body.className = "shower";
     return () => {
@@ -43,16 +41,14 @@ export default function Presentation({ content, frontMatter, ...props }) {
   );
 }
 
-export const getStaticProps = async (pr) => {
+export const getStaticProps = async () => {
   const source = fs.readFileSync(`./index.mdx`);
-
   const { content, data } = matter(source);
 
   const mdxSource = await renderToString(content, {
     components,
     mdxOptions: {
       remarkPlugins: [
-        require("remark-mark-plus"),
         [require("remark-attr"), { enableAtxHeaderInline: false }],
         require("remark-sectionize"),
       ],
@@ -66,12 +62,9 @@ export const getStaticProps = async (pr) => {
       ${data.subtitle && `<p>${data.subtitle}</p>`}
     </header>`;
 
-  const presentationFooter = `
-    <footer class="badge">
-        <a href="https://github.com/shower/shower">Fork me on GitHub</a>
-    </footer>
-
-    <div class="progress"></div>`;
+  const presentationFooter = `${
+    data.footer && data.footer
+  }<div class="progress"></div>`;
 
   const presHtml =
     presentationHeader + mdxSource.renderedOutput + presentationFooter;
