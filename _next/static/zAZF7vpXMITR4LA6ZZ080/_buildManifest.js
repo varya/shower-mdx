2151e2c4ed871a561eddd6d2098f261a6a23206d
varya <varya@users.noreply.github.com>
2021-02-16 13:09:10 +0000

    deploy: 006b04a973dfdbc4cd1feabcdc186245b675b0b8

---
+++ _next/static/zAZF7vpXMITR4LA6ZZ080/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0bafab10e5737e937269.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-926211a5891cf52c1d97.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
